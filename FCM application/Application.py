adbd254b14c55b4936e04cb2522587acdd4c4d91
phuchoang29 <31571856+phuchoang92@users.noreply.github.com>
2021-12-10 17:18:29 +0700

    Update Application.py

--- FCM application/Application.py
+++ FCM application/Application.py
@@ -5,19 +5,32 @@ from PyQt5.QtCore import Qt
 from FCM1 import FCM1
 from PyQt5 import uic
 
+class TableModel(QtCore.QAbstractTableModel):
 
+    def __init__(self, data):
+        super(TableModel, self).__init__()
+        self._data = data
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            value = self._data[index.row(), index.column()]
+            return str(value)
+
+    def rowCount(self, index):
+        return self._data.shape[0]
+
+    def columnCount(self, index):
+        return self._data.shape[1]
 
 class MyWindowClass(QMainWindow):
+    
     def __init__(self):
         super(MyWindowClass, self).__init__()
         uic.loadUi("FCM application\main_window.ui", self)
         
-        
-        self.k_text.setText("fdfsdfdsf")
-
-
-
-
+        self.fcm1 = FCM1()
+        self.fcm2 = FCM2()
+        self.run_btn.clicked.connect(self.caculate_cluster)
 
 
     def choose_data(self):
@@ -26,13 +39,41 @@ class MyWindowClass(QMainWindow):
             path, _ = QFileDialog.getOpenFileName(parent=self, caption='Select a Data File', filter=file_filter,
                                                   options=QFileDialog.DontUseNativeDialog)
             
+            self.data_path.setText(path)
             self.fcm1.read_data(path)
+            model_x = TableModel(self.fcm1.X)
+            self.X_table.setModel(model_x)
+            self.X_table.resizeColumnsToContents()
+            
         except:
             self.error_dialog = QtWidgets.QErrorMessage()
             self.error_dialog.showMessage('Please choose a data set !')
     def caculate_cluster():
-        return
-app = QApplication(sys.argv)
-myWindow = MyWindowClass()
-myWindow.show()
-app.exec_()
+        
+        if self.number_of_label.text() != '' and (self.number_of_cluster.text() != '') and (self.epsilon.text() != ''):
+            number_of_label = int(self.k_text.text())
+            number_of_cluster = int(self.k_text.text())
+            epsilon = float(self.epsilon_text.text())
+
+            if self.algo1_check.isChecked(): #Kiểm tra có chạy thuật toán 1 hay ko
+                value_of_m = int(self.m_text.text())
+                if self.check_2pha_algo1.isChecked():
+                    self.fcm1.thuat_toan_2_pha(epsilon, value_of_m, number_of_cluster, number_of_label)
+                else:
+                    self.fcm1.thuat_toan_1_pha(epsilon,value_of_m,number_of_cluster,number_of_label)
+
+            if self.algo2_check.isChecked(): #Kiểm tra có chạy thuật toán 2 hay ko
+                value_of_M = int(self.M_text.text())
+                value_of_M1 = int(self.M1_text.text())
+
+                if self.check_2pha_algo2.isChecked():
+       
+def main():
+    app = QApplication([])
+    mainWindow = App()
+    mainWindow.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
